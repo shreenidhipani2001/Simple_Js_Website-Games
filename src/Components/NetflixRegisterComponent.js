94436151de4176cff05e32468a874f1eed8e695e
SHREENIDHI PANI <143885806+shreenidhipani2001@users.noreply.github.com>
2024-07-22 22:03:45 +0530

    Add files via upload

---
+++ src/Components/NetflixRegisterComponent.js
@@ -0,0 +1,17 @@
+export function NetflixRegisterComponent(){
+    return(
+        <div className="container-fluid mt-3 text-center bg-dark p-3 text-white w-75">
+            <p>Ready to watch? Enter your email to create or restart your membership?</p>
+            <div className="input-group input-group-lg ">
+                <input type="email" className="form-control" />
+                <button className="btn btn-danger">
+                    Get Started
+                    <span className="bi bi-chevron-right">
+
+                    </span>
+                </button>
+            </div>
+        </div>
+        
+    );
+}
